4047b953e9b39bdac28a617469c4c8683de7a3e2
Viet Hoang Pham <104506968@student.swin.edu.au>
2025-03-15 13:02:24 +1100

    Add user_id field to image/folder/label

--- models/folder.py
+++ models/folder.py
@@ -1,7 +1,9 @@
 ﻿from sqlmodel import SQLModel, Field, Relationship
-from typing import List
+from typing import List, Optional
 from backend.models.folder_label import FolderLabel
 import uuid
+
+from backend.models.user import User
 
 
 class Folder(SQLModel, table=True):
@@ -9,7 +11,9 @@ class Folder(SQLModel, table=True):
 
     folder_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str = Field(nullable=False)
+    user_id: Optional[uuid.UUID] = Field(foreign_key="user.user_id")
 
+    user: User = Relationship(back_populates="user")
     images: List["Image"] = Relationship(back_populates="folder")
     labels: List["Label"] = Relationship(
         back_populates="folder",
--- models/label.py
+++ models/label.py
@@ -3,14 +3,19 @@ from typing import Optional, List
 from backend.models.folder_label import FolderLabel
 import uuid
 
+from backend.models.user import User
+
+
 class Label(SQLModel, table=True):
     __tablename__ = "label"
 
     label_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str = Field(nullable=False)
     color: Optional[str] = Field(nullable=True)
+    user_id: Optional[uuid.UUID] = Field(foreign_key="user.user_id")
 
     folders: List["Folder"] = Relationship(
         back_populates="label",
         link_model=FolderLabel
     )
+    user: User = Relationship(back_populates="user")
